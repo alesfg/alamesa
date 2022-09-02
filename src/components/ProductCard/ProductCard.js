27ac3e454a2f55d8fde7dff379c86c8f801d2e6d
alesfg <alex.f.yo@hotmail.com>
2022-09-02 13:44:48 +0200

    create productcard structure

---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ProductCard.css";
+
+function ProductCard() {
+  return (
+    <div className="container">
+      <input type="search" placeholder="Buscar" className="productcard" />
+    </div>
+  );
+}
+
+export default ProductCard;
